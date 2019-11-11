f5ecf3df7fc007396c328f057d6ee613c14587c2
vohyz <531322336@qq.com>
2019-11-11 23:56:37 +0800

    feature - 更新订单搜索

--- venv/main.py
+++ venv/main.py
@@ -30,6 +30,31 @@ def main():
 def search():
     return render_template("search.html")
 
+@app.route("/search/id", methods=['GET', 'POST'])
+def searchid():
+    form_data = request.form
+    order_id = form_data.get("ordertext")
+    print(order_id)
+    conn,cursor = connect_mysql()                       # 连接到mysql
+    sql = 'SELECT * FROM `order` WHERE `order_id` = %s'%order_id
+    cursor.execute(sql) 
+    order = cursor.fetchone()
+    print(order)
+    Data = {}
+    if order:
+        Data = {
+            'order_id':order[0],
+            'begin_time':order[1],
+            'begin_name':order[2],
+            'begin_phone':order[3],
+            'end_name':order[4],
+            'end_phone':order[5],
+            'begin_city':order[6],
+            'end_city':order[7],
+            'order_state':order[8]
+            }
+    return render_template("search.html", Data = Data)
+
 @app.route("/city")
 def city():
     return render_template("city.html")
@@ -37,10 +62,46 @@ def city():
 @app.route("/create")
 def create():
     return render_template("create.html")
-
-@app.route("/create/order")
+@app.route("/create/failed")
+def failed():
+    return render_template("createfailed.html")
+@app.route("/create/success")
+def success():
+    return render_template("createsuccess.html")
+@app.route("/create/order", methods=['GET', 'POST'])
 def order():
-    return render_template("create.html")
+    begin_name = request.form.get('order_begin_name')
+    begin_phone = request.form.get('order_begin_phone')
+    begin_city = request.form.get('order_begin_city')
+    end_name = request.form.get('order_end_name')
+    end_phone = request.form.get('order_end_phone')
+    end_city = request.form.get('order_end_city')
+    '''
+    此处可能需要支付
+    '''
+    conn,cursor = connect_mysql()                       # 连接到mysql
+
+    sql = 'SELECT max(`order_id`) from `order`'
+    cursor.execute(sql) 
+    orders = cursor.fetchone()
+    neworder_id = int(orders[0]) + 1
+    '''
+    此处需要发送给calculate
+
+    获得返回信息后
+    '''
+    try:
+        sql='INSERT into `order` VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        args = (pymysql.escape_string(str(neworder_id)),pymysql.escape_string(t),pymysql.escape_string(begin_name),pymysql.escape_string(begin_phone),pymysql.escape_string(end_name),pymysql.escape_string(end_phone),pymysql.escape_string(begin_city),pymysql.escape_string(end_city),pymysql.escape_string('进行中'))
+        cursor.execute(sql,args)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect("/create/success")
+    except Exception as e:
+        return redirect("/create/failed")
+    
 
 @app.route('/login')
 def login():
@@ -48,7 +109,6 @@ def login():
     
 @app.route('/login/in', methods=['GET', 'POST'])    
 def login_in():
-    print('login')
     json_data = request.json                            # 获取数据
     userphone = json_data.get("mobile")
     sms_code = json_data.get("sms_code")
@@ -71,6 +131,7 @@ def login_in():
                 else:                                   # 如不存在则存入数据库再登录
                     sql = 'INSERT into user VALUES (%s,%s,%s)'
                     args = ('Null', 'Null', userphone)
+                    cursor.execute(sql, args)
                     session['userphone'] = userphone
                 conn.commit()
                 cursor.close()
@@ -154,10 +215,15 @@ def getaliyun(phonenumber, code):#调用阿里云去发送短信
     response = client.do_action(request)
     print(str(response, encoding = 'utf-8'))
 
-if __name__ == '__main__':
+if __name__ == '__main__':   
     '''
     conn,cursor = connect_mysql()
-    sql='truncate table user'
-    cursor.execute(sql)
+    sql='INSERT into `order` VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    args = (pymysql.escape_string('100000'),pymysql.escape_string(t),pymysql.escape_string('1号测试人员'),pymysql.escape_string('15316172791'),pymysql.escape_string('2号测试人员'),pymysql.escape_string('13131313131'),pymysql.escape_string('上海'),pymysql.escape_string('北京'),pymysql.escape_string('进行中'))
+    cursor.execute(sql,args)
+    conn.commit()
+    cursor.close()
+    conn.close()
     '''
     app.run(debug = True, host= '0.0.0.0')
