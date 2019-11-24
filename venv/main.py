e361708b7ea72d5103e3c9eb1df3fffd352f751b
vohyz <531322336@qq.com>
2019-11-24 17:41:19 +0800

    feature - 微服务全部分离

--- venv/main.py
+++ venv/main.py
@@ -7,13 +7,9 @@ ZYX
 
 from flask import Flask, jsonify, request, make_response, url_for,redirect, render_template, session, Session
 from flask_httpauth import HTTPBasicAuth
-from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.request import CommonRequest
 import datetime
-import pymysql
-import random
+import pika
 import os
-import requests
 import json, urllib
 from urllib import parse
 from urllib.request import urlopen
@@ -32,97 +28,128 @@ def main():
 
 @app.route("/order/<order_id>")
 def showOrder(order_id):
-    order = []
     try:
-        conn,cursor = connect_mysql()                       # 连接到mysql
-        sql = 'SELECT * FROM `order` WHERE `order_id` = %s'
-        args = (order_id)
-        cursor.execute(sql, args)
-        order = cursor.fetchone()
-        conn.commit()
-        cursor.close()
-        conn.close()
+        url = "http://127.0.0.1:5001/searchbyOrder"
+        params = {
+            'order_id': order_id
+        }
+        params = parse.urlencode(params) 
+
+        f = urlopen("%s?%s" % (url, params))
+
+        content = f.read()
+        res = json.loads(content)
     except:
         Data = {
-            'error_message': 'error'
+            'error_message': 'error1'
         }
         return render_template("order.html", Data = Data)
-    # print(order)
-    times = order[9].split()
-    order_time = []
-    i = 0
-    while i < len(times):
-        order_time.append(times[i] +' '+ times[i+1])
-        i += 2
-    order_place = order[10].split()
-    order_details = order[11]
-    print(len(order_time))
-    print(order_time)
-    n = len(order_time)
-    Data = {
-        'order_id': order_id,
-        'begin_time_1': order[1][:11],
-        'begin_time_2': order[1][11:],
-        'begin_name': order[2],
-        'begin_phone': order[3],
-        'end_name': order[4],
-        'end_phone': order[5],
-        'begin_city': order[6],
-        'end_city': order[7],
-        'order_state': order[8],
-        'order_time': order_time,
-        'order_place': order_place[:n],
-        'order_details': order_details,
-        'n': n
-    }
+    message = res['order_message']
+    if message != '0':
+        order = message['orders'][0]
+        times = order['order_transport_time'].split()
+        order_time = []
+        i = 0
+        while i < len(times):
+            order_time.append(times[i] +' '+ times[i+1])
+            i += 2
+        order_place = order['order_transport_place'].split()
+        # order_details = order[11]
+        print(len(order_time))
+        print(order_time)
+        n = len(order_time)
+        Data = {
+            'order_id':order['order_id'],
+            'begin_time_1':order['begin_time_1'],
+            'begin_time_2':order['begin_time_2'],
+            'begin_name':order['begin_name'],
+            'begin_phone':order['begin_phone'],
+            'end_name':order['end_name'],
+            'end_phone':order['end_phone'],
+            'begin_city':order['begin_city'],
+            'end_city':order['end_city'],
+            'order_state':order['order_state'],
+            'error_message': '0',
+            'order_time': order_time,
+            'order_place': order_place[:n],
+            'n': n
+        }
+    else:
+        Data = {
+            'error_message': 'error2'
+        }
     return render_template("order.html", Data = Data)
 
 @app.route("/search")
-def search():
+def search():                                           # 订单查询页
     return render_template("search.html")
 
 @app.route("/search/id", methods=['GET', 'POST'])
-def searchid():
+def searchid():                                         # 订单查询页搜索
     form_data = request.form
     order_id = form_data.get("ordertext")
-    print(order_id)
-    conn,cursor = connect_mysql()                       # 连接到mysql
-    sql = 'SELECT * FROM `order` WHERE `order_id` = %s'%order_id
-    cursor.execute(sql) 
-    order = cursor.fetchone()
-    print(order)
+
     Data = {}
-    if order:
+    try:
+        url = "http://127.0.0.1:5001/searchbyOrder"
+        params = {
+            'order_id': order_id
+        }
+        params = parse.urlencode(params)
+
+        f = urlopen("%s?%s" % (url, params))
+
+        content = f.read()
+        res = json.loads(content)
+    except:
         Data = {
-            'order_id':order[0],
-            'begin_time_1':order[1][:11],
-            'begin_time_2':order[1][11:],
-            'begin_name':order[2],
-            'begin_phone':order[3],
-            'end_name':order[4],
-            'end_phone':order[5],
-            'begin_city':order[6],
-            'end_city':order[7],
-            'order_state':order[8]
+            'error_message': 'error1'
+        }
+        return render_template("search.html", Data = Data)
+    orders = res['order_message']['orders']
+    # print(orders)
+    if orders != '0':
+        Data = {
+            'error_message': '0',
+            'orders':[
+                {
+                'order_id':order['order_id'],
+                'begin_time_1':order['begin_time_1'],
+                'begin_time_2':order['begin_time_2'],
+                'begin_name':order['begin_name'],
+                'begin_phone':order['begin_phone'],
+                'end_name':order['end_name'],
+                'end_phone':order['end_phone'],
+                'begin_city':order['begin_city'],
+                'end_city':order['end_city'],
+                'order_state':order['order_state']
+                }
+            for order in orders]
             }
+        # print(Data)
+    else:
+        Data = {
+            'error_message': 'error2'
+        }
     return render_template("search.html", Data = Data)
 
 @app.route("/city")
-def city():
+def city():                                             # 城市查询页
     return render_template("city.html")
 
 @app.route("/create")
-def create():
+def create():                                           # 创建订单页路由
     return render_template("create.html")
 @app.route("/create/failed")
-def failed():
+def failed():                                           # 创建订单后失败页
     return render_template("createfailed.html")
 @app.route("/create/success")
-def success():
+def success():                                          # 创建订单后成功页
+    # 添加一个显示创建成功的订单的详细信息的页面
     return render_template("createsuccess.html")
 
 @app.route("/create/order", methods=['GET', 'POST'])
-def order():
+def order():                                            # 创建订单提交
     begin_name = request.form.get('order_begin_name')
     begin_phone = request.form.get('order_begin_phone')
     begin_city = request.form.get('order_begin_city')
@@ -132,8 +159,11 @@ def order():
     '''
     此处可能需要支付
     ''' 
+
+    # 此处调用订单模块的api
+
     try:
-        url = "http://127.0.0.1:5001"
+        url = "http://127.0.0.1:5001/create"
         params = {
             'order_begin_name': begin_name,
             'order_begin_phone': begin_phone,
@@ -156,173 +186,140 @@ def order():
     except Exception as e:
         print(e)
         return redirect("/create/failed")
-    
+
 @app.route('/login')
-def login():
+def login():                                            # 登录界面路由
     return render_template('login.html')
-    
+
 @app.route('/login/in', methods=['GET', 'POST'])    
-def login_in():
+def login_in():                                         # 用户输入信息登录
     json_data = request.json                            # 获取数据
     userphone = json_data.get("mobile")
     sms_code = json_data.get("sms_code")
 
-    conn,cursor = connect_mysql()                       # 连接到mysql
-
-    try:                                                # 判断验证码是否通过
-        sql = 'SELECT phonenumber,code from sms_code'
-        cursor.execute(sql)
-        p_code = cursor.fetchall()
-        #print(p_code)
-        #print((userphone, sms_code) )
-        if (userphone, sms_code) in p_code:             # 验证码通过
-            try:                                        # 判断手机是否已被注册
-                sql = 'SELECT user_phone from user'
-                cursor.execute(sql)
-                phonenumbers = cursor.fetchall()
-                if (userphone,) in phonenumbers:        # 如果手机号已存在，则直接登录
-                    session['userphone'] = userphone
-                else:                                   # 如不存在则存入数据库再登录
-                    sql = 'INSERT into user VALUES (%s,%s,%s)'
-                    args = ('Null', 'Null', userphone)
-                    cursor.execute(sql, args)
-                    session['userphone'] = userphone
-                conn.commit()
-                cursor.close()
-                conn.close()
-                session.permanent = True
-                app.permanent_session_lifetime = datetime.timedelta(minutes=10)
-                return jsonify(errno='ok', errmsg="登录成功")
-            except Exception as e:
-                return jsonify(errno='notok', errmsg="用户数据读取失败")
-        else:                                           # 验证码不通过
-            return jsonify(errno='notok', errmsg="验证码错误")
+    try:
+        url = "http://127.0.0.1:5004/login"
+        params = {
+            'userphone': userphone,
+            "sms_code": sms_code
+        }
+        params = parse.urlencode(params)
+        
+        f = urlopen("%s?%s" % (url, params))
+        
+        content = f.read()
+        res = json.loads(content)
+        print(res)  
+        if res['errno'] == 'ok':
+            session['userphone'] = userphone
+            session.permanent = True
+            app.permanent_session_lifetime = datetime.timedelta(minutes=10)
+            return jsonify(errno='ok', errmsg="登录成功")
+        else:
+            return jsonify(errno='notok', errmsg=res['errmsg'])
     except Exception as e:
-        return jsonify(errno='notok',errmsg="数据库查询错误")
+        #print(e)
+        return jsonify(errno='notok', errmsg="用户数据读取失败")
 
 @app.route('/sms_code', methods=['GET', 'POST'])
 def sms_code():                                         # 发送验证码
     json_data = request.json
     mobile = json_data.get("mobile")
-
-    conn,cursor = connect_mysql()                       # 连接到mysql
-
-    try:                                                # 判断手机是否已被注册
-        sql = 'SELECT user_phone from user'
-        cursor.execute(sql)
-        phonenumbers = cursor.fetchall()
-        if (mobile,) in phonenumbers:
-            return jsonify(errno='dataexist',errmsg="该手机号已经被注册")
-    except Exception as e:
-        return jsonify(errno='databaseerror',errmsg="数据库查询错误")
-
-    result = random.randint(0, 999999)                  # 生成验证码
-    sms_code = "%06d" % result
-    print("验证码：{}".format(sms_code))
-
-    
-    try:                                                # 调用阿里云去发送短信
-        getaliyun(mobile, sms_code)
-    except Exception as e:
-        return jsonify(errno='databaseerror', errmsg="发送短信失败")
-
-    try:                                                # 将手机号和验证码存入数据库
-        sql = 'SELECT phonenumber from sms_code'
-        cursor.execute(sql)
-        phonenumbers = cursor.fetchall()
-        if (mobile,) in phonenumbers:
-            sql = 'UPDATE sms_code SET code = %s WHERE phonenumber = %s'
-            args = (sms_code, mobile)
+    try:
+        url = "http://127.0.0.1:5004/sms"
+        params = {
+            'userphone': mobile
+        }
+        params = parse.urlencode(params)
+        
+        f = urlopen("%s?%s" % (url, params))
+        
+        content = f.read()
+        res = json.loads(content)
+        print(res)  
+        if res['errno'] == 'ok':
+            return jsonify(errno=res['errno'], errmsg=res['errmsg'])
         else:
-            sql = 'INSERT into sms_code VALUES (%s,%s)'
-            args = (mobile, sms_code)
-        result = cursor.execute(sql, args)
-        conn.commit()
-        cursor.close()
-        conn.close()
-    except Exception as e:
-        return jsonify(errno='codestoreerror', errmsg="手机验证码保存失败")
-
+            return jsonify(errno=res['errno'], errmsg=res['errmsg'])
+    except:
+        return jsonify(errno='error', errmsg="发送失败")
     return jsonify(errno='ok', errmsg="发送成功")
 
 @app.route('/usercenter')
-def usercenter():
+def usercenter():                                       # 用户个人中心
     if 'userphone' in session:
         userphone = session['userphone']
     else:
         return redirect('/')
-    conn,cursor = connect_mysql()                       # 连接到mysql
-    sql = 'SELECT * from user where user_phone = %s'%userphone
-    cursor.execute(sql) 
-    user = cursor.fetchone()
+    # 此处整合到登录注册模块
+    # //
+    Data = {}
+    try:
+        url = "http://127.0.0.1:5001/searchbyUser"
+        params = {
+            'userphone': userphone
+        }
+        params = parse.urlencode(params)
 
-    sql = 'SELECT * from `order` where begin_user_phone = %s'%userphone
-    cursor.execute(sql) 
-    orders = cursor.fetchall()
-    conn.commit()
-    cursor.close()
-    conn.close()
-    Data =[]
-    if orders:
+        f = urlopen("%s?%s" % (url, params))
+
+        content = f.read()
+        res = json.loads(content)
+    except:
         Data = {
-            'user':user[0],
+            'user':userphone,
             'user_phone':userphone,
+            'error_message': 'error1'
+        }
+        return render_template("usercenter.html", Data = Data)
+    if res['order_message'] != '0':
+        orders = res['order_message']['orders']
+    else:
+        orders = 'error'
+    if orders != 'error':
+        Data = {
+            'user':userphone,
+            'user_phone':userphone,
+            'error_message': '0',
             'orders':[
                 {
-            'order_id':order[0],
-            'begin_time_1':order[1][:11],
-            'begin_time_2':order[1][11:],
-            'begin_name':order[2],
-            'begin_phone':order[3],
-            'end_name':order[4],
-            'end_phone':order[5],
-            'begin_city':order[6],
-            'end_city':order[7],
-            'order_state':order[8],
+            'order_id':order['order_id'],
+            'begin_time_1':order['begin_time_1'],
+            'begin_time_2':order['begin_time_2'],
+            'begin_name':order['begin_name'],
+            'begin_phone':order['begin_phone'],
+            'end_name':order['end_name'],
+            'end_phone':order['end_phone'],
+            'begin_city':order['begin_city'],
+            'end_city':order['end_city'],
+            'order_state':order['order_state'],
             } 
             for order in orders]
             }
+    else:  
+        Data = {
+            'user':user[0],
+            'user_phone':userphone,
+            'error_message': 'error'
+        }
     return render_template('usercenter.html', Data = Data)
 
 @app.route('/logout')
-def logout():
+def logout():                                           # 用户登出函数
     session.pop('userphone', None)
     return redirect('/')
 
-def connect_mysql():#链接mysql
-    conn = pymysql.connect(host="cdb-518aglpe.bj.tencentcdb.com", port=10101, user="root", password="zyx1999zyx", database="service")
-    cursor = conn.cursor() 
-    return conn,cursor
-
-def getaliyun(phonenumber, code):#调用阿里云去发送短信
-    client = AcsClient('LTAIyNuWycN5rsY5', 'vRqO3bw3o93LmvD9m24W12bK2InFHn', 'cn-hangzhou')
-    request = CommonRequest()
-    request.set_accept_format('json')
-    request.set_domain('dysmsapi.aliyuncs.com')
-    request.set_method('POST')
-    request.set_protocol_type('https') # https | http
-    request.set_version('2017-05-25')
-    request.set_action_name('SendSms')
-
-    request.add_query_param('RegionId', "cn-hangzhou")
-    request.add_query_param('PhoneNumbers', phonenumber)
-    request.add_query_param('SignName', "vohyz")
-    request.add_query_param('TemplateCode', "SMS_177242135")
-    request.add_query_param('TemplateParam', "{\"code\":\"%s\"}"%code)
-
-    response = client.do_action(request)
-    print(str(response, encoding = 'utf-8'))
 
 if __name__ == '__main__':   
-    '''
-    conn,cursor = connect_mysql()
-    sql='INSERT into `order` VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    args = (pymysql.escape_string('100000'),pymysql.escape_string(t),pymysql.escape_string('1号测试人员'),pymysql.escape_string('15316172791'),pymysql.escape_string('2号测试人员'),pymysql.escape_string('13131313131'),pymysql.escape_string('上海'),pymysql.escape_string('北京'),pymysql.escape_string('进行中'))
-    cursor.execute(sql,args)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    '''
     app.run(debug = True, host= '0.0.0.0')
+    '''userphone = '15316172791'
+    url = "http://127.0.0.1:5004/sms"
+    params = {
+        'userphone': userphone
+    }
+    params = parse.urlencode(params)
     
+    f = urlopen("%s?%s" % (url, params))
+    content = f.read()
+    res = json.loads(content)'''
+    # print(res)  
